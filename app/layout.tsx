280b9414b2a50e01b3d5f30c68164649e71f13e3
Vivi <vivi@privacyrequired.com>
2024-11-09 19:58:55 -0500

    Update layout component metadata and restructure body elements for improved clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,3 @@
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-
 import '@/app/globals.css'
 import { cn } from '@/lib/utils'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
@@ -11,8 +8,8 @@ import { Toaster } from '@/components/ui/sonner'
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Gem City Chatbot',
+    template: `%s - Gem City XYZ`
   },
   description: 'An AI-powered chatbot template built with Next.js and Vercel.',
   icons: {
@@ -24,8 +21,8 @@ export const metadata = {
 
 export const viewport = {
   themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' }
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: '(prefers-color-scheme: light)', color: 'white' }
   ]
 }
 
@@ -36,17 +33,10 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          'font-sans antialiased',
-          GeistSans.variable,
-          GeistMono.variable
-        )}
-      >
-        <Toaster position="top-center" />
+      <body>
         <Providers
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           // enableSystem
           disableTransitionOnChange
         >
@@ -55,8 +45,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
           </div>
           <TailwindIndicator />
+          <Toaster position="top-center" />
         </Providers>
-      </body>
+        </body>
     </html>
   )
 }
