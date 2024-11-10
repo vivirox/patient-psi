dea3a5db71adcc7a59beb2b7077faed29063b0ec
Vivi <vivi@privacyrequired.com>
2024-11-09 19:51:28 -0500

    Update Next.js configuration to change image remote patterns and restructure export format

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
-export const images = {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'patient.gemcity.xyz',
-      port: '',
-      pathname: '**'
-    }
-  ]
-};
-export const serverRuntimeConfig = {
-  logger: {
-    level: 'info',
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '**'
+      }
+    ]
   },
-  serverPath: 'app/api',
-};
+  serverRuntimeConfig: {
+    logger: {
+      level: 'info',
+    },
+    serverPath: 'app/api',
+  }
+}
